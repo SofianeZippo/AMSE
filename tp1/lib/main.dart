90a2116ec0ed5d50ae896ec7a6cf56dd23eb972f
SZ <sofianezippo@gmail.com>
2025-02-24 00:25:32 +0100

    HomePage+FavorisSimple+SearchTrèsSimple

--- tp1/lib/main.dart
+++ tp1/lib/main.dart
@@ -1,132 +1,321 @@
+import 'package:english_words/english_words.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:flutter/services.dart' show rootBundle;
+import 'package:csv/csv.dart'; //nécessaire pour lire les fichiers csv où sont renseignées les données
+import 'dart:math'; //nécessaire afin de manipuler l'aléatoire
+/*nombreRandom = Random().nextInt(200)*/
+
 
 void main() {
-  runApp(const MyApp());
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a purple toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
+    return ChangeNotifierProvider(
+      create: (context) => MyAppState(),
+      child: MaterialApp(
+        title: 'Namer App',
+        theme: ThemeData(
+          useMaterial3: true,
+          colorScheme: ColorScheme.fromSeed(seedColor: Colors.blueAccent),
+        ),
+        home: MyHomePage(),
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
     );
   }
 }
 
+class MyAppState extends ChangeNotifier {
+  var current = WordPair.random();
+
+  void getNext() {
+    current = WordPair.random();
+    notifyListeners();
+  }
+  
+  var favorites = <int>[];
+
+  void toggleFavorite(int index) {
+    if (favorites.contains(index)) {
+      favorites.remove(index);
+    } else {
+      favorites.add(index);
+    }
+    notifyListeners();
+  }
+}
+
 class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
   @override
   State<MyHomePage> createState() => _MyHomePageState();
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+  var selectedIndex = 0;
 
-  void _incrementCounter() {
+  List<List<dynamic>> csvData = [];
+  int IndexData = 0;
+
+  @override
+  void initState() {
+    super.initState();
+    loadCsvData();
+  }
+
+  Future<void> loadCsvData() async {
+    final rawData = await rootBundle.loadString('assets/data.csv');
+    final listData = CsvToListConverter().convert(rawData);
+
     setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
+      csvData = listData;
     });
   }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            ElevatedButton(
-            onPressed: () {
-              print('bouton pressé!');
-            },
-            child: Text('Suivant'),
-          ),
+    Widget page;
+    switch (selectedIndex) {
+      case 0:
+        page = GeneratorPage(csvData: csvData, IndexData: IndexData);
+        break;
+      case 1:
+        page = PageFavoris(csvData: csvData, IndexData: IndexData);
+        break;
+      case 2:
+        page = PageRecherche();
+        break;
+      default:
+        throw UnimplementedError('no widget for $selectedIndex');
+    }
 
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+    return LayoutBuilder(
+      builder: (context, constraints) {
+        return Scaffold(
+          appBar: AppBar(
+            title: Text("Saphir"),
+          ),
+          body: Row(
+            children: [
+              Expanded(
+                child: Container(
+                  color: Theme.of(context).colorScheme.primaryContainer,
+                  child: page,
+                ),
+              ),
+            ],
+          ),
+          // Placer la barre de navigation en bas
+          bottomNavigationBar: BottomNavigationBar(
+            currentIndex: selectedIndex,
+            onTap: (value) {
+              setState(() {
+                selectedIndex = value;
+              });
+            },
+            items: const [
+              BottomNavigationBarItem(
+                icon: Icon(Icons.home),
+                label: 'Accueil',
+              ),
+              BottomNavigationBarItem(
+                icon: Icon(Icons.favorite),
+                label: 'Favoris',
+              ),
+              BottomNavigationBarItem(
+                icon: Icon(Icons.search),
+                label: 'Rechercher',
+              ),
+            ],
+          ),
+        );
+      },
     );
+  }
+}
+
+class GeneratorPage extends StatelessWidget {
+  final List<List<dynamic>> csvData;
+  int IndexData;
+
+  GeneratorPage({required this.csvData, required this.IndexData}); 
+
+  @override
+  Widget build(BuildContext context) {
+    var appState = context.watch<MyAppState>();
+
+    IconData icon;
+    if (appState.favorites.contains(IndexData)) {
+      icon = Icons.favorite;
+    } else {
+      icon = Icons.favorite_border;
+    }
+
+    String imageName = csvData[IndexData][4];
+
+    return Column(
+      children: [
+        SizedBox(height: 40),
+
+        // Grand Titre et Sous-titre
+        Text(
+          'Bienvenue sur Saphir',
+          style: TextStyle(
+            fontSize: 32,
+            fontWeight: FontWeight.bold,
+            color: Colors.white,
+            shadows: [
+              Shadow(
+                blurRadius: 10.0,
+                color: Colors.black,
+                offset: Offset(3.0, 3.0),
+              ),
+            ],
+          ),
+        ),
+        SizedBox(height: 10),
+
+        Center(
+          child: Text(
+          'Découvrez de nouvelles oeuvres ou répertoriez celles que vous aimez déjà !!',
+          textAlign: TextAlign.center,
+          style: TextStyle(
+            fontSize: 20,
+            color: Colors.white,
+            shadows: [
+              Shadow(
+                blurRadius: 10.0,
+                color: Colors.black,
+                offset: Offset(3.0, 3.0),
+              ),
+            ],
+          ),
+        ),),
+        
+        SizedBox(height: 60),
+
+        Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Card(
+                color: Theme.of(context).colorScheme.secondaryFixedDim,
+                elevation: 36,
+                child: Padding(
+                  padding: const EdgeInsets.all(20),
+                  child: Column(
+                    children: [
+                      Image.asset(
+                        'assets/images/${imageName}',
+                        width: 200,
+                        height: 200,
+                      ),
+                      Text(
+                        '${csvData[IndexData][0]}',
+                        style: Theme.of(context).textTheme.displayMedium!.copyWith(color: Theme.of(context).colorScheme.onSecondary),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+              SizedBox(height: 10),
+              Row(
+                mainAxisSize: MainAxisSize.min,
+                children: [
+                  ElevatedButton.icon(
+                    onPressed: () {
+                      appState.toggleFavorite(IndexData);
+                    },
+                    icon: Icon(icon),
+                    label: Text("J'aime"),
+                  ),
+                  SizedBox(width: 10),
+                  ElevatedButton(
+                    onPressed: () {
+                      appState.getNext();
+                      IndexData < csvData.length - 1
+                          ? IndexData++
+                          : IndexData = 0;
+                    },
+                    child: Text('Suivant'),
+                  ),
+                ],
+              ),
+            ],
+          ),
+        ),
+      ],
+    );
+  }
+}
+
+class PageFavoris extends StatelessWidget {
+  final List<List<dynamic>> csvData;
+  final int IndexData;
+
+  PageFavoris({required this.csvData, required this.IndexData});
+
+  
+
+  @override
+  Widget build(BuildContext context) {
+    var appState = context.watch<MyAppState>();
+    final theme = Theme.of(context);
+    final style = theme.textTheme.displayMedium!.copyWith(color: theme.colorScheme.onSecondary);
+    
+
+    if (appState.favorites.isEmpty) {
+      return Center(child: Text('Oh! Aucun Favoris ajoutés pour le moment :(', style: style,textAlign: TextAlign.center,));
+    }
+
+    return ListView(
+      children: [
+        Padding(
+          padding: const EdgeInsets.all(20),
+          child: Text('Vous avez ${appState.favorites.length} favoris:'),
+        ),
+        for (var num in appState.favorites)
+          //var String imageName = csvData[num][4];
+          ListTile(
+            leading: Image.asset(
+              'assets/images/${csvData[num][4]}',
+              width: 40,
+              height: 40,
+            ),
+            title: Text('${csvData[num][0]}'),
+            trailing: ElevatedButton(
+              onPressed: () {
+                ScaffoldMessenger.of(context).showSnackBar(
+                  SnackBar(content: Text('Bouton pressé!')),
+                );
+              },
+              child: Text('détails'),
+            ),
+          ),
+      ],
+    );
+  }
+}
+
+class PageRecherche extends StatelessWidget {
+  PageRecherche();
+  @override
+  Widget build(BuildContext context) {
+
+    return Column(
+        children: [
+          Text('Cherchez un Média'),
+          SearchBar(
+            leading: Icon(Icons.search),
+            hintText: 'Recherchez une oeuvre dans notre catalogue',
+            overlayColor: WidgetStateProperty.all(const Color.fromARGB(255, 243, 246, 243)),
+            backgroundColor: WidgetStateProperty.all(const Color.fromARGB(255, 150, 217, 217)),
+        ),
+            ],
+            );
   }
 }
